4d7df87a65327a0b9a27a75bdb0c7d8fecd71793
Theo5607 <theo67.jeu@gmail.com>
2023-12-04 15:44:20 +0100

    template

---
+++ templ.ml
@@ -0,0 +1,19 @@
+let siter = String.iter
+let siteri = String.iteri
+let aiter = Array.iter
+let aiteri = Array.iteri
+let liter = List.iter
+let literi = List.iteri
+
+let rec read_lines f =
+  try
+    let line = input_line f in
+    line :: read_lines f
+  with End_of_file -> []
+
+let soc c s =
+  String.split_on_char c s |> Array.of_list
+
+let () =
+  let file = read_lines (open_in "tay.txt") |> Array.of_list in
+  Array.iter print_string file
